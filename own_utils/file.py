4e7412bde883c0cc7a673df93a1925a0c67bfb3b
Alexandre Fournier Montgieux <alexandre.fourniermontgieux@cea.fr>
2023-04-25 12:03:12 +0200

    add tar compression

--- own_utils/file.py
+++ own_utils/file.py
@@ -1,3 +1,7 @@
+import tarfile
+from .paths import flatten_paths_recursively
+import os
+
 def extract_file_lines_to_list(file_path:str, skip_blank_line=True):
     """
     Extract each lines of a file into a list and ignore \n
@@ -11,3 +15,15 @@ def extract_file_lines_to_list(file_path:str, skip_blank_line=True):
     if skip_blank_line:
         result = list(filter(lambda elt: elt!='', result))
     return result
+
+def compress_to_tar(target = './', file_tree_level=0,replace_existing = False, remove_init_folder = False):
+    content_to_tar_paths = flatten_paths_recursively(target, depth=file_tree_level)
+    
+    for elt_path in content_to_tar_paths:
+        if replace_existing or not(os.path.exists(elt_path + '.tar')):
+            with tarfile.open(elt_path+ '.tar', "w") as tf:
+                arcname = elt_path.spit('/')[-1]
+                tf.add(elt_path, arcname=arcname)
+        else:
+            print("file already existing : ", elt_path+ '.tar')
+        
