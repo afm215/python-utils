a95bd573d1c0d0ee11b5cbaf0d1ba2a3b3a74b40
Alexandre Fournier Montgieux <alexandre.fourniermontgieux@cea.fr>
2023-05-15 15:11:35 +0200

    patch

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name = 'own_utils',
       version = own_utils.__version__ , 
       packages = find_packages(exclude=['*.tests.*', 'waiting_features']),
       # install_requires = ['os'],
-	long_description = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read())
+	long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read())
       #  packages_dir = {'' : 'les_packages_maison'})
 
