92baa63c4931257181b248f4cf9457fe0c44e67f
Alexandre Fournier Montgieux <alexandre.fourniermontgieux@cea.fr>
2023-07-27 10:17:39 +0200

    oredrlist util

--- own_utils/__init__.py
+++ own_utils/__init__.py
@@ -1,4 +1,5 @@
-__version__='0.1.1'
+__version__='0.1.2'
+# This version introduces OrderedList
 from . import basics
 from . import paths
 from . import bash_command
---
+++ own_utils/maths/list.py
@@ -0,0 +1,76 @@
+from enum import Enum
+class _SortMode(Enum):
+    DEFAULT=0
+    INDEX = 1 
+    KEY = 2
+
+class OrderedList:
+
+    def _dichotomy_insert(self, list_: list, new_elt, new_elt_sort_value) -> list:
+        if len(list_) == 0:
+            return [new_elt]
+        elt = list_[0]
+        elt_sort_value = elt if self.sort_mode == _SortMode.DEFAULT else elt[self.sort_on]
+        if elt_sort_value > new_elt_sort_value:
+            return [new_elt] + list_
+        if len(list_) == 1:
+            return [elt, new_elt_sort_value]
+
+        half_list_length = len(list_) // 2
+        splited_list_1, splited_list_2 = list_[:half_list_length], list_[half_list_length:]
+        elt = splited_list_2[0]
+        elt_sort_value = elt if self.sort_mode == _SortMode.DEFAULT else elt[self.sort_on]   
+        if new_elt_sort_value< elt_sort_value:
+            return self._dichotomy_insert(splited_list_1, new_elt, new_elt_sort_value) + splited_list_2
+        return splited_list_1 + self._dichotomy_insert(splited_list_2, new_elt, new_elt_sort_value)
+
+    
+    def append(self, new_elt):
+        new_elt_sort_value = new_elt if self.sort_mode == _SortMode.DEFAULT else new_elt[self.sort_on]
+        if self.insert_mode == "dichotomy":
+            self.list_ = self._dichotomy_insert(self.list_, new_elt, new_elt_sort_value)
+            return
+
+        for i, elt in enumerate(self.list_):
+            elt_sort_value = elt if self.sort_mode == _SortMode.DEFAULT else elt[self.sort_on]   
+            if new_elt_sort_value< elt_sort_value:
+                self.list_.insert(i, new_elt) 
+                return
+        self.list_.append(new_elt)
+        return
+
+
+    def __init__(self, init_list: 'list | None' = None, sort_on: 'int | None | str'= None, insert_mode: 'str | None' = "dichotomy"):
+        self.list_ = []
+        self.insert_mode = insert_mode
+        self.current_index = 0
+        if sort_on is None:
+            self.sort_mode = _SortMode.DEFAULT
+        elif type(sort_on) == int:
+            self.sort_mode = _SortMode.INDEX
+            self.sort_on = sort_on
+        elif type(sort_on) == str:
+            self.sort_mode = _SortMode.KEY
+            self.sort_on = sort_on
+        else:
+            raise TypeError("Type Error on sort_on")
+        if not(init_list is None):
+            for elt in init_list:
+                self.append(elt)
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+      if self.current_index < len(self.list_):
+          self.current_index += 1
+          return self.list_[self.current_index - 1] 
+      else:
+          raise StopIteration
+    def __getitem__(self, index):
+        return self.list_[index]
+
+    def __str__(self) -> str:
+        return str(self.list_)
+    def __len__(self):
+        return len(self.list_)
