027647169b882ff24f3a5cd8ced160cb3fd1563e
Alexandre Fournier Montgieux <alexandre.fourniermontgieux@cea.fr>
2023-08-01 19:43:13 +0200

    fix

--- own_utils/__init__.py
+++ own_utils/__init__.py
@@ -3,3 +3,6 @@ __version__='0.1.5'
 # OrderedList : correct dichotomy_function typo, add verbose , and returns an OrderedList instance when giving slice to an OrderedList object
 # get_relative_path : normalize data_path to avoid truncature of the wanted relative path
 # ToPILImageWrapper : correctly formated class dexcription 
+from . import paths
+from . import basics
+from . import bash_command
